754bd13a05298a12c587aa0719b4db9feedafd22
LNSSPsd <lnsspsd@madisoncollege.edu>
2018-08-22 12:54:11 +0800

    First

---
+++ mwc.js
@@ -0,0 +1,66 @@
+try{
+const wes=require("ws");
+const wss=new wes.Server({port:19133});
+}catch(errst){
+console.log("Error when loading ws: %s.",errst.message);l
+process.exit(1);
+}
+
+var clients=[];
+
+ws.on("connection",function (ws){
+ws.send(JSON.stringify({
+		"body": {
+			"eventName": "PlayerMessage"
+		},
+		"header": {
+			"requestId": "0ffae098-00ff-ffff-abbbbbbbbbdf3344",
+			"messagePurpose": "subscribe",
+			"version": 1,
+			"messageType": "commandRequest"
+		}
+	}));
+
+function gamecmds(cmd) {
+		ws.send(JSON.stringify({
+			"body": {
+				"origin": {
+					"type": "player"
+				},
+				"commandLine": cmd,
+				"version": 1
+			},
+			"header": {
+				"requestId": "00000000-0001-0000-000000000000",
+				"messagePurpose": "commandRequest",
+				"version": 1,
+				"messageType": "commandRequest"
+			}
+		}));
+	}
+  ws.on("message",function (msg){
+  if (JSON.parse(message).body.eventName == "PlayerMessage"
+		/* && JSON.parse(message).body.properties.MessageType=="chat"*/
+		&& JSON.parse(message).header.requestId != "00000000-0001-0000-000000000000") {
+			if (JSON.parse(message).body.properties.Message.substring(0,10) == "*/connect ") {
+       var con=JSON.parse(message).body.properties.Message.split(" ")[1];
+       try{
+       var wsc=new wes(con);
+       wsc.on("open",function(){clients.push(wsc);gamecmds("say Connected to websocket: "+con);});
+       wsc.on("message",function(msgg){ws.send(msgg);}
+       }catch(erro){
+        gamecmds("say Failed to connect to websocket: "+erro.message);
+        return;
+      }
+      return;
+}
+clients.forEach(function(wscc,i){
+wscc.send(msg);
+});
+
+
+  });
+  
+  });
+  
+
