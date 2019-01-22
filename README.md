76ad46359d196ffc0b95ce7bf0a937750f896856
LNSSPsd <24724395+LNSSPsd@users.noreply.github.com>
2019-01-22 16:59:32 +0800

    Update xD

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # MWC
-A tool to connect multi websocket at once time on Minecraft:BE.
+A tool to connect multi websocket at once time on Minecraft:BE.  
 通过它，您可以在Minecraft基岩版中同时连接多个WebSocket服务器（基岩版只允许同时一个连接）
 ## Author
-LNSSPsd
+[LNSSPsd](https://github.com/LNSSPsd)
--- mwc.js
+++ mwc.js
@@ -16,7 +16,10 @@ console.log("Error when loading ws: %s.",errst.message);
 process.exit(1);
 }
 
-var clients=[];
+
+process.stdin.resume();
+process.on("SIGINT",function(){wss.close();console.log("Websocket Closed.");});
+//var clients=[];
 
 wss.on("connection",function (ws){
 ws.send(JSON.stringify({
@@ -32,6 +35,7 @@ ws.send(JSON.stringify({
 	}));
 
 function gamecmds(cmd) {
+	try{
 		ws.send(JSON.stringify({
 			"body": {
 				"origin": {
@@ -46,18 +50,61 @@ function gamecmds(cmd) {
 				"version": 1,
 				"messageType": "commandRequest"
 			}
-		}));
+		}));}catch(undefined){}
+	}
+
+
+	var wss=[];
+	var idp=1;
+
+	function findid(id){
+		var fdd=undefined;
+		wss.forEach(function(e,i){
+			if(e.id==id)fdd=e;
+		});
+		return fdd;
+	}
+
+	function killall(){
+		wss.forEach(function(e,i){try{e.terminate()}catch(undefined){}});
 	}
 	
-	gamecmds("say MWC Connected!");
+	gamecmds("say §2§lMWC Connected!");
 	console.log("Client Connected!");
 	
   ws.on("message",function (msg){
   if (JSON.parse(msg).body.eventName == "PlayerMessage"
 		/* && JSON.parse(message).body.properties.MessageType=="chat"*/
 		&& JSON.parse(msg).header.requestId != "00000000-0001-0000-000000000000") {
-			if (JSON.parse(msg).body.properties.Message.substring(0,10) == "*/connect ") {	
-				switch (JSON.parse(msg).body.properties.Message.substring(10, 40)) {
+	  var spl=JSON.parse(msg).body.properties.Message.split(" ");
+	  if(JSON.parse(msg).body.properties.Message.split(" ")[0]=="*/dc"||spl[0]=="*/disconnect"){
+		  try{findid(spl[1]).ws.terminate();gamecmds("say Done");}catch(undefined){gamecmds("say Unable to disconnected.\nMaybe: Server is disconnected.");}return;
+	  }
+	  if(spl[0]=="*/dcm"){
+		  try{ws.terminate()}catch(undefined){}
+		  wss.forEach(function(e,i){try{e.ws.close();}catch(undefined){}});
+		  wss=[];
+		  return;
+	  }
+	  if(spl[0]=="*/reset"||spl[0]=="*/killall"){
+		  killall();
+		  idp=0;
+		  wss=[];
+		  gamecmds("say Done");
+	  }
+	  if(spl[0]=="*/listid"){
+		  wss.forEach(function(e,i){
+			  try{
+				  e.ws.send("");
+				  gamecmds("say "+e.id+":"+e.ip);
+			  }catch(undefined){}
+		  });
+		  gamecmds("IDPool: "+idp);
+		  return;
+	  }
+	  
+			if (spl[0] == "*/connect") {	
+				switch (spl[1]) {
 					case "127.0.0.1:" + portm :	
 						console.log("Bad IP!");
 						gamecmds("say We don't suggest you to connect to self.");
@@ -77,9 +124,11 @@ function gamecmds(cmd) {
 						}
 						var wsc=new wes(con);
 						wsc.on("open",function(){
-							clients.push(wsc);
-							gamecmds("say Connected to websocket: "+con);
-							console.log("say Connected to websocket: "+con);
+							//clients.push(wsc);
+							wss.push({id:idp,ws:wsc,ip:wsc.url});
+							gamecmds("say Connected to websocket: "+con+",ID:"+idp);
+							idp++;
+							console.log("Connected to websocket: "+con);
 						});
 						wsc.on("message",function(msgg){ws.send(msgg);});
 						wsc.on("error",function(err){
@@ -104,8 +153,8 @@ function gamecmds(cmd) {
 				return;
 			}
 		}
-clients.forEach(function(wscc,i){
-wscc.send(msg);
+wss.forEach(function(wscc,i){
+try{wscc.ws.send(msg);}catch(undefined){}
 });
 
 
